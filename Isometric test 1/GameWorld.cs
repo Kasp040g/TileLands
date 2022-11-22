8893c7ff3c7112999dd5081168e9f7f3e490ef58
MTBonde <thorbonde@gmail.com>
2022-11-22 16:37:13 +0100

    render test

--- Isometric test 1/GameWorld.cs
+++ Isometric test 1/GameWorld.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
+using System;
 using System.Collections.Generic;
 
 namespace Isometric_test_1
@@ -10,10 +12,17 @@ namespace Isometric_test_1
         //Setup essential variables for the game
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        // Rendering and Texture
+        private RenderTarget2D _renderBuffer;   // will draw before it is drawn on screen
+        private Rectangle _renderRectangle;     // where rendertarget2d will be drawn 
+        //private Texture2D _texture;
+
+        //init Gamemanager
         private GameManager _gameManager;
 
-        public static int ScreenWidth = 1280;
-        public static int ScreenHeight = 720;
+        public static int ScreenWidth = 640;
+        public static int ScreenHeight = 480;
 
         private List<ScrollingBackground> _scrollingBackgrounds;
 
@@ -28,6 +37,9 @@ namespace Isometric_test_1
         {
             //Update graphics device manager
             _graphics = new GraphicsDeviceManager(this);
+
+            //allow window rezizing
+            Window.AllowUserResizing = true;
 
             //Update content root directory
             Content.RootDirectory = "Content";
@@ -45,9 +57,17 @@ namespace Isometric_test_1
         /// </summary>
         protected override void Initialize()
         {
+            //insta. Render buffer
+            _renderBuffer = new RenderTarget2D(GraphicsDevice, 1920, 1080);       // rendertarget is 1920, 1080
+
+            // Delegate method that will run when window size is changed            
+            Window.ClientSizeChanged += OnWindowSizeChange;     // add and subscribe to deleagte method, will be called when window is changed
+            OnWindowSizeChange(null, null);                     // (no object, no event)
+
             //Set game window size
             _graphics.PreferredBackBufferWidth = ScreenWidth;
             _graphics.PreferredBackBufferHeight = ScreenHeight;
+            _graphics.IsFullScreen = false;
             _graphics.ApplyChanges();
 
             //Transfer content to be global
@@ -60,6 +80,28 @@ namespace Isometric_test_1
             base.Initialize();
         }
 
+        private void OnWindowSizeChange(object sender, EventArgs e)
+        {
+            //first we find screen width and hight
+            var width = Window.ClientBounds.Width;
+            var height = Window.ClientBounds.Height;
+
+            // calculate if we want pillar or letterboxing to fit _renderBuffer on screen
+            if(height < width / (float)_renderBuffer.Width * _renderBuffer.Height)          // (skærm bred / render bred) * render høj større end skærm høj hvis den er større kan det ikke passe på skræmen og vi resizer
+            {
+                width = (int)(height / (float)_renderBuffer.Height * _renderBuffer.Width);  // tegne bred = (skræm høj / render høj) * rende bred  (pillarbox)  ELLER hvis mindre 
+            }
+            else
+            {
+                height = (int)(width / (float)_renderBuffer.Width * _renderBuffer.Height); // tegne høj = (skræm bred/ render bred) * render høj (letterbox)
+            }
+
+            // calculate new x,y by taking half of screen hight, width and moving screen to fit
+            // (new screen size width - render buffer rectangle width) / 2
+            var x = (Window.ClientBounds.Width - width) / 2;
+            var y = (Window.ClientBounds.Height - height) / 2;
+            _renderRectangle = new Rectangle(x, y, width, height);
+        }
 
         /// <summary>
         /// Loads the contents of the game once
@@ -70,16 +112,16 @@ namespace Isometric_test_1
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             _scrollingBackgrounds = new List<ScrollingBackground>()
-           {
-        new ScrollingBackground(Content.Load<Texture2D>("BackGrounds/Clouds_Fast"), 18f, true)
-        {
-          Layer = 0.99f,
-        },
-       
-        new ScrollingBackground(Content.Load<Texture2D>("BackGrounds/Clouds_Slow"), 25f, true)
-        {
-          Layer = 0.8f,
-        } };
+            {
+                new ScrollingBackground(Content.Load<Texture2D>("BackGrounds/Clouds_Fast"), 18f, true)
+                {
+                    Layer = 0.99f,
+                },
+                new ScrollingBackground(Content.Load<Texture2D>("BackGrounds/Clouds_Slow"), 25f, true)
+                {
+                    Layer = 0.8f,
+                }
+            };
         
 
             //Transfer sprite batch to be global
@@ -135,21 +177,51 @@ namespace Isometric_test_1
         /// </summary>
         /// <param name="gameTime"></param>
         protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+        {   
+            // sets GD to draw to render buffer
+            GraphicsDevice.SetRenderTarget(_renderBuffer);      
+            // clear screen to color
+            GraphicsDevice.Clear(Color.Black);
 
+            // tell SB what sorting mode, and clamping
             _spriteBatch.Begin(SpriteSortMode.FrontToBack, null, SamplerState.PointClamp);
 
-            foreach (var sb in _scrollingBackgrounds)
+            // foreach for running the parallaxing background 
+            foreach(var sb in _scrollingBackgrounds)
                 sb.Draw(gameTime, _spriteBatch);
 
             _spriteBatch.End();
 
+            // then we draw the gamemanager(the game itself)
             _spriteBatch.Begin();
             _gameManager.Draw();
             _spriteBatch.End();
 
+            // Reset rendertarget to null and tell Draw we want to draw to the screen
+            GraphicsDevice.SetRenderTarget(null);                               
+            GraphicsDevice.Clear(Color.HotPink);
+
+            _spriteBatch.Begin();
+            // we draw all that is put in render buffer to screen
+            _spriteBatch.Draw(_renderBuffer, _renderRectangle, Color.White);    
+            _spriteBatch.End();
+
             base.Draw(gameTime);
+
+            //GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //_spriteBatch.Begin(SpriteSortMode.FrontToBack, null, SamplerState.PointClamp);
+
+            //foreach(var sb in _scrollingBackgrounds)
+            //    sb.Draw(gameTime, _spriteBatch);
+
+            //_spriteBatch.End();
+
+            //_spriteBatch.Begin();
+            //_gameManager.Draw();
+            //_spriteBatch.End();
+
+            //base.Draw(gameTime);
         }
     }
 }
