b726fa30f0299e8f23c332057933b907fab5888a
MTBonde <thorbonde@gmail.com>
2022-11-30 21:09:35 +0100

    minor fixes

--- Isometric test 1/States/GameState.cs
+++ Isometric test 1/States/GameState.cs
@@ -6,19 +6,18 @@ namespace Isometric_test_1
         // Instantiate map
         public static Map _map = new();
 
-        //Animations
-        private Eagle _bird_ss = new(new(Globals.Bounds.Y / 2, 100));        
+               
 
         public override void Update(GameManager gm)
         {
             _map.Update();
-            _bird_ss.Update();
+            
         }
 
         public override void Draw(GameManager gm)
         {
             _map.Draw();
-            _bird_ss.Draw();
+            
         }
     }
 }
