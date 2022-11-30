c5ff9559247964b900ae989ceef07098cd909f02
MTBonde <thorbonde@gmail.com>
2022-11-30 20:10:30 +0100

    Changes: GW, GM, IM Added: SM, Btn, MS,GS,SS,S

---
+++ Isometric test 1/GameState.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Isometric_test_1
+{
+    public class GameState
+    {
+        // Instantiate map
+        public static Map _map = new();
+
+        //Animations
+        private Eagle _bird_ss = new(new(Globals.Bounds.Y / 2, 100));
+    }
+}
